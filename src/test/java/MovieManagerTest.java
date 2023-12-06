e34056965260aeb92bbe2d58380107114395bd8a
SemNik <142649558+SemNik88@users.noreply.github.com>
2023-12-06 17:05:46 +0300

    Update MovieManagerTest.java

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -1,71 +1,59 @@
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MovieManagerTest {
 
     @Test
-    public void testAddMovie() {
-        MovieManager manager = new MovieManager(3);
+    public void testFindLastWhenMoviesLessThanLimit() {
+        MovieManager manager = new MovieManager(5);
         manager.addMovie("Movie 1");
         manager.addMovie("Movie 2");
-        manager.addMovie("Movie 3");
-        Movie[] movies = manager.findAll();
 
+        Movie[] lastMovies = manager.findLast();
         Movie[] expected = {
-                new Movie("Movie 1"),
                 new Movie("Movie 2"),
-                new Movie("Movie 3")
+                new Movie("Movie 1")
         };
 
-        assertArrayEquals(expected, movies);
+        assertArrayEquals(expected, lastMovies);
     }
 
     @Test
-    public void testFindAllEmpty() {
-        MovieManager manager = new MovieManager();
-        Movie[] movies = manager.findAll();
-        assertEquals(0, movies.length);
-    }
-
-    @Test
-    public void testFindLastDefaultLimit() {
-        MovieManager manager = new MovieManager();
+    public void testFindLastWhenMoviesMoreThanLimit() {
+        MovieManager manager = new MovieManager(3);
         manager.addMovie("Movie 1");
         manager.addMovie("Movie 2");
         manager.addMovie("Movie 3");
         manager.addMovie("Movie 4");
         manager.addMovie("Movie 5");
-        manager.addMovie("Movie 6");
 
         Movie[] lastMovies = manager.findLast();
         Movie[] expected = {
-                new Movie("Movie 6"),
                 new Movie("Movie 5"),
                 new Movie("Movie 4"),
-                new Movie("Movie 3"),
-                new Movie("Movie 2")
+                new Movie("Movie 3")
         };
 
         assertArrayEquals(expected, lastMovies);
     }
 
     @Test
-    public void testFindLastCustomLimit() {
-        MovieManager manager = new MovieManager(2);
+    public void testFindLastWhenMoviesEqualToLimit() {
+        MovieManager manager = new MovieManager(4);
         manager.addMovie("Movie 1");
         manager.addMovie("Movie 2");
         manager.addMovie("Movie 3");
+        manager.addMovie("Movie 4");
 
         Movie[] lastMovies = manager.findLast();
         Movie[] expected = {
+                new Movie("Movie 4"),
                 new Movie("Movie 3"),
-                new Movie("Movie 2")
+                new Movie("Movie 2"),
+                new Movie("Movie 1")
         };
 
         assertArrayEquals(expected, lastMovies);
     }
 }
-
 
